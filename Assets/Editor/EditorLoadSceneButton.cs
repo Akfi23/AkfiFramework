d9902e4489d39bf440acec3a6e2d867028f7f27f
Andrey <60118284+Akfi23@users.noreply.github.com>
2024-06-10 17:22:14 +0300

    [ADD] Editor extensions

---
+++ Assets/Editor/EditorLoadSceneButton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityToolbarExtender;
+
+//using UnityToolbarExtender;
+
+namespace Editor
+{
+
+    [InitializeOnLoad]
+    public class EditorLoadSceneButton
+    {
+        static class ToolbarStyles
+        {
+            public static readonly GUIStyle CommandButtonStyle;
+
+            static ToolbarStyles()
+            {
+                CommandButtonStyle = new GUIStyle("Command")
+                {
+                    fontSize = 10,
+                    alignment = TextAnchor.MiddleCenter,
+                    imagePosition = ImagePosition.TextOnly,
+                    fontStyle = FontStyle.Bold,
+                    fixedWidth = 70f
+                };
+            }
+        }
+
+        static EditorLoadSceneButton()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(LeftToolbarGUI);
+        }
+
+        private static void LeftToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+
+            GUI.enabled = !EditorApplication.isPlaying;
+
+            GUI.backgroundColor = Color.cyan;
+
+            if (GUILayout.Button(new GUIContent("SCENE", "Select scene"), ToolbarStyles.CommandButtonStyle, GUILayout.Width(100f)))
+            {
+                SelectSceneEditorWindow.OpenWindow();
+            }
+
+            GUI.backgroundColor = Color.white;
+            GUI.enabled = true;
+        }
+    }
+}
